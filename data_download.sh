0d563ead967a05fae360a726602b59ba722dd85c
yanhan-si <syanhan96@gmail.com>
2020-11-26 19:29:03 -0500

    Create data_download.sh

---
+++ data_download.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env zsh
+
+# 79: -------------------------------------------------------------------------
+#
+# To download the required datasets using: `zsh ./data_download.sh`
+#
+# Author(s): Yanhan Si
+# Updated: Nov 26, 2020
+# 79: -------------------------------------------------------------------------
+
+# wget https://wwwn.cdc.gov/Nchs/Nhanes/1999-2000/TELO_A.XPT -P data
+# wget https://wwwn.cdc.gov/Nchs/Nhanes/1999-2000/DEMO.XPT -P data
+wget https://wwwn.cdc.gov/Nchs/Nhanes/1999-2000/BMX.XPT -P data
